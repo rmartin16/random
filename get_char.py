4ec51339abe5c2909cc5f79c87ff94e09c37c575
russell <rmartin16@gmail.com>
2019-04-28 17:36:04 -0400

    use os.name to default platform; remote timeout sanity checks for windows; make imports more efficient

--- get_char.py
+++ get_char.py
@@ -2,48 +2,52 @@
 class _GetChar:
     """Gets a single character from standard input.  Does not echo to the screen."""
     def __init__(self):
-        try:
+        from os import name as osname
+        if osname == 'nt':
             self.impl = _GetCharWindows()
-        except ImportError:
+        else:
             self.impl = _GetCharUnix()
 
+    # TODO: add support for a default value...it s a little trickier than
+    #       it looks since the char may not be printable e.g. the up arrow key
     def __call__(self, timeout=None):
-        ch = self.impl(timeout)
+        char = self.impl(timeout)
         # catch CTRL+C (needed for non-windows)
-        if ch == '\x03':
+        if char == '\x03':
             raise KeyboardInterrupt
-        return ch
+        return char
 
 
 class _GetCharUnix:
     """Fetch and character using the termios module."""
     def __init__(self):
-        import tty, sys
-        from select import select
+        pass
 
     def __call__(self, timeout):
-        import sys, tty, termios
+        from sys import stdin
+        from tty import setraw
+        from termios import tcgetattr, tcsetattr, TCSADRAIN
         from select import select
 
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
+        fd = stdin.fileno()
+        old_settings = tcgetattr(fd)
 
         try:
-            tty.setraw(sys.stdin.fileno())
+            setraw(stdin.fileno())
 
             # [ Wait until ready for reading,
             #   wait until ready for writing
             #   wait for an "exception condition" ]
             # The below line times out after 1 second
             # This can be changed to a floating-point value if necessary
-            [i, _, _] = select([sys.stdin.fileno()], [], [], timeout)
+            [i, _, _] = select([stdin.fileno()], [], [], timeout)
             if i:
-                ch = sys.stdin.read(1)
+                ch = stdin.read(1)
             else:
                 ch = None
 
         finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            tcsetattr(fd, TCSADRAIN, old_settings)
 
         return ch
 
@@ -51,25 +55,21 @@ class _GetCharUnix:
 class _GetCharWindows:
     """Fetch a character using the Microsoft Visual C Runtime."""
     def __init__(self):
-        import msvcrt
+        pass
 
     def __call__(self, timeout):
-        import msvcrt
-        import time
+        from msvcrt import getch, kbhit
+        from time import sleep
 
         # just block for input if no timeout
         if timeout is None:
-            return msvcrt.getch()
+            return getch()
 
         else:  # otherwise continuously check for input until timeout
-            try:
-                timeout = int(timeout)
-                for _ in range(0, timeout):
-                    if msvcrt.kbhit():  # is input waiting?
-                        return msvcrt.getch()  # return that input
-                    time.sleep(1)
-            except ValueError:
-                pass  # ensure timeout is an integer...
+            for _ in range(0, timeout):
+                if kbhit():  # is input waiting?
+                    return getch()  # return that input
+                sleep(1)
         return
 
 
