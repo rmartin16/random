6163c33777cef674f6914fe8f5b7117968c8b836
russell <rmartin16@gmail.com>
2019-04-28 15:23:45 -0400

    small fixes and tweaks

--- run_nmon.py
+++ run_nmon.py
@@ -14,37 +14,48 @@ class Timeout(Exception):
     pass
 
 
+class GetOutOfHere(Exception):
+    pass
+
+
 def run_with_input():
     proc = None
-    commands = ['l', 'n', 'g', 't']
+    all_commands = 'cmdrkhljnNtVvb0u.1345'
+    top_process_commands = '1345'
+    user_commands = ['l', 'n', 'g', 't']
     while True:
         try:
             start_time = time.time()
-            proc = subprocess.Popen(['nmon', '-g', '/home/user/.nmon_disk_group'], stdin=subprocess.PIPE) #, stdout=subprocess.PIPE)
+            proc = subprocess.Popen(['nmon', '-g', '/home/user/.nmon_disk_group'], stdin=subprocess.PIPE)  # , stdout=subprocess.PIPE)
 
             # commands may be populated if we're just restarting nmon
             # so, send the commands to restore state
-            for command in commands:
+            for command in user_commands:
                 os.write(proc.stdin.fileno(), str(command).encode())
 
             while True:
                 # get next command if any
                 user_char = get_char(1)
 
-                # treat q as ctrl+c to force us all the way out
+                # treat q as special to force us all the way out "properly"
                 if user_char == 'q':
-                    raise Exception
+                    raise GetOutOfHere
 
-                # logs user input and send the char to nmon
+                # send command to nmon and log the command
                 if user_char is not None:
-                    #proc.stdin.write(user_char.encode())
+                    # proc.stdin.write(user_char.encode())
                     os.write(proc.stdin.fileno(), user_char.encode())
 
                     # keep track of state so it can be restored when nmon is restarted
-                    if user_char in commands:
-                        commands.remove(user_char)
-                    else:
-                        commands.append(user_char)
+                    if user_char in all_commands:
+                        if user_char in user_commands:
+                            if user_char not in top_process_commands:
+                                user_commands.remove(user_char)
+                        else:
+                            if user_char in top_process_commands and 't' in user_commands:
+                                user_commands.append(user_char)
+                            else:
+                                user_commands.append(user_char)
 
                 # check if nmon needs to be restarted
                 if (time.time() - start_time) > timeout:
@@ -53,10 +64,13 @@ def run_with_input():
         # and start back over
         except Timeout:
             kill(proc)
+        # exit out of everything
+        except GetOutOfHere:
+            break
 
-        # clean up and exit on *any* exception (otherwise, screen is hosed...)
-        except BaseException as e:
-            exit_prog(proc)
+        # clean up on the way out (otherwise, screen is hosed...)
+        finally:
+            exit_program(proc)
 
 
 def run_naked():
@@ -66,23 +80,24 @@ def run_naked():
             proc.communicate(input=b'lngt', timeout=timeout)
         except subprocess.TimeoutExpired:
             kill(proc)
-        except KeyboardInterrupt:
-            exit_prog(proc)
+        finally:
+            exit_program()
 
 
-def kill(proc=None):
-    if proc is not None:
-        process = psutil.Process(proc.pid)
+def kill(input_process=None):
+    if input_process is not None:
+        process = psutil.Process(input_process.pid)
         for proc in process.children(recursive=True):
             proc.terminate()
         process.terminate()
 
 
-def exit_prog(proc=None):
+def exit_program(proc=None):
     kill(proc)
-    subprocess.call('reset')
+    subprocess.call('reset')  # reset terminal since it always seems to be messed up after...
     print()
-    sys.exit(0)
+    # sys.exit(0)
 
 
 run_with_input()
+# run_naked()
