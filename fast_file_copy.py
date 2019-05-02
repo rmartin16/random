7350c3fb9d7dd1f78b0f1720a44e6be8c4796e7f
russell <rmartin16@gmail.com>
2019-05-02 14:12:43 -0400

    added documentation and put getsize in try block

--- fast_file_copy.py
+++ fast_file_copy.py
@@ -3,10 +3,23 @@ import sys
 
 
 def copy_file(src, dst, progress_bar=True):
+    """
+    Copy file 'src' to 'dst'
+
+    Note:
+     src and dst must be fully qualified, absolute, and expanded filepaths.
+     dst must actually be an actual file...not just a path to the destination directory
+
+    :param src: file to be copied
+    :param dst: destination for new file
+    :param progress_bar: True to print a progress bar
+    :return: 0 for success else failure
+    """
+
+    # initialize
     return_value = 0
     file_in = None
     file_out = None
-
     try:
         O_BINARY = os.O_BINARY
     except AttributeError:
@@ -15,12 +28,12 @@ def copy_file(src, dst, progress_bar=True):
     WRITE_FLAGS = os.O_WRONLY | os.O_CREAT | os.O_TRUNC | O_BINARY
     BUFFER_SIZE = 128*1024
 
-    # remove if progress bar isn't needed
-    expected_loop_count = os.path.getsize(src) / BUFFER_SIZE
-    (loop_count, progress_count) = (0, 0)
-
-    # copy the file - TODO: implement handling for different error cases
     try:
+        # remove if progress bar isn't needed
+        expected_loop_count = os.path.getsize(src) / BUFFER_SIZE
+        (loop_count, progress_count) = (0, 0)
+
+        # copy the file - TODO: implement handling for different error cases
         file_in = os.open(src, READ_FLAGS)
         stat = os.fstat(file_in)
         file_out = os.open(dst, WRITE_FLAGS, stat.st_mode)
@@ -30,7 +43,8 @@ def copy_file(src, dst, progress_bar=True):
             if progress_bar:
                 (loop_count, progress_count) = manage_progress_bar(expected_loop_count, loop_count, progress_count)
 
-    except OSError:
+    except OSError as e:
+        print(e)
         return_value = 1
     finally:
         for fd in [file_in, file_out]:
