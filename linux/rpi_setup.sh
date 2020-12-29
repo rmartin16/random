3591cb62be0b15ec75be5054aa39c04c41d99122
Russell Martin <rmartin16@gmail.com>
2020-12-28 22:29:49 -0500

    Update rpi_setup.sh

--- linux/rpi_setup.sh
+++ linux/rpi_setup.sh
@@ -8,19 +8,6 @@ echo "russell ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/010_russell-
 
 # ssh back in as russell for additional setup
 
-# bash aliases
-printf "alias ls='ls -lh --color=auto'\nalias la='ls -A'\n\nalias dc='docker-compose'\n" | tee -a ~/.bash_aliases
-source ~/.bash_aliases
-
-# set up vim
-sudo apt install -y git
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime && sh ~/.vim_runtime/install_awesome_vimrc.sh
-# repeat for root with sudo -i
-
-# set up prompt
-curl -fsSL https://raw.githubusercontent.com/rmartin16/random/master/bash/.bash_prompt.sh -o ~/.bash_prompt
-printf "\nif [ -f ~/.bash_prompt ]; then\n    . ~/.bash_prompt\nfi\n" | tee -a ~/.bashrc
-
 # raspbian OS config
 raspi-config
 # change hostname
@@ -28,9 +15,30 @@ raspi-config
 # set GPU memory to 16
 # update locale and timezone
 # expand filesystem
+# boot latest ROM
+
+# restart
 
 # update machine
 sudo apt update && sudo apt upgrade -y
+
+# set up ssh keys
+if [ ! -f ~/.ssh/id_rsa ]; then ssh-keygen; fi && cat ~/.ssh/id_rsa.pub && read -n 1 -r -s -p $'\nAdd key to NAS and press enter to continue...\n'
+# allow connections to NAS
+ssh-keyscan -H 10.16.8.1 >> ~/.ssh/known_hosts
+
+# bash aliases
+printf "alias ls='ls -lh --color=auto'\nalias la='ls -A'\n\nalias dc='docker-compose'\n" | tee -a ~/.bash_aliases
+source ~/.bash_aliases
+
+# set up vim
+sudo apt install -y git vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime && sh ~/.vim_runtime/install_awesome_vimrc.sh
+# repeat for root with sudo -i
+
+# set up prompt
+curl -fsSL https://raw.githubusercontent.com/rmartin16/random/master/bash/.bash_prompt.sh -o ~/.bash_prompt
+printf "\nif [ -f ~/.bash_prompt ]; then\n    . ~/.bash_prompt\nfi\n" | tee -a ~/.bashrc && source ~/.bash_prompt
 
 # set up automatic updates
 sudo apt-get install -y unattended-upgrades
@@ -54,11 +62,6 @@ sudo make install
 printf "# Filter kernel image filenames by regex. This is required on Raspian having\n# multiple kernel image variants installed in parallel.\n\$nrconf{kernelfilter} = qr(kernel7l\.img);\n" | sudo tee -a /etc/needrestart/conf.d/kernel.conf
 cd $SAVEPWD
 
-# set up ssh keys
-if [ ! -f ~/.ssh/id_rsa ]; then ssh-keygen; fi && cat ~/.ssh/id_rsa.pub && read -n 1 -r -s -p $'\nAdd key to NAS and press enter to continue...\n'
-# allow connections to NAS
-ssh-keyscan -H 10.16.8.1 >> ~/.ssh/known_hosts
-
 # set up NAS mounts
 sudo apt-get -y install sshfs
 sudo mkdir -p /mnt/nas/Media
@@ -76,3 +79,9 @@ curl -fsSL https://get.docker.com | sudo bash
 sudo usermod -aG docker russell
 sudo apt-get install -y libffi-dev libssl-dev python3-dev python3 python3-pip
 python3 -m pip install docker-compose
+# set up regular clean ups
+printf "  0 3    *   *   *   /mnt/nas/Data/scripts/cleanup_docker.sh 2>&1 | /mnt/nas/Data/scripts/datetime_output_to_logfile.sh >> /home/russell/logs/docker_cleanup.log" | sudo tee -a /etc/cron.d/cleanup _docker
+
+# set up backups
+mkdir -p ~/logs
+printf "  0 5    *   *   *   /mnt/nas/Data/scripts/backup_to_ds1515.sh 2>&1 | /mnt/nas/Data/scripts/datetime_output_to_logfile.sh >> /home/russell/logs/backup_to_ds1515.log" | sudo tee -a /etc/cron.d/backup_to_ds1515
